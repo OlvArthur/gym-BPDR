957dc6d282d8f1395a99dc5332785f141ad98446
Arthur Pereira <apereira@bpdl.com>
2025-11-28 09:09:19 -0500

    add anonymous auth and analytics config into firebase

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,5 +1,6 @@
-// import { getAnalytics } from 'firebase/analytics';
+import { getAnalytics, logEvent } from 'firebase/analytics';
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -10,13 +11,20 @@ const firebaseConfig = {
   messagingSenderId: "101709513054",
   appId: "1:101709513054:web:2576e68d0925ef2403cfcf",
   measurementId: "G-H392GFFQPX"
-};
+}
 
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 // For more information on how to access Firebase,
 // see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
+const analytics = getAnalytics(app)
+logEvent(analytics, 'firestore_error')
+logEvent(analytics, 'app_open')
 
 
-export const db = getFirestore(app);
-// const analytics = getAnalytics(app);
+export const db = getFirestore(app)
+
+const auth = getAuth(app)
+signInAnonymously(auth).catch((error) => {
+  console.error("Firebase auth error:", error)
+})
